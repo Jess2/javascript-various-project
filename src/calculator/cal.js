db025c045c93dadae7f1c8c87cdaf9d54fbd9f14
jessie <je_ss2@naver.com>
2018-04-20 05:48:16 +0900

    코드 정리

--- src/calculator/cal.js
+++ src/calculator/cal.js
@@ -1,60 +1,38 @@
 window.onload = function() {
-    var inp = document.forms['cal']; //name이 'frm'인 form객체를 선택
-    var inputs = cal.getElementsByTagName('input'); //태그명이 input인 문서 객체를 지정. 배열로 참조됨
-    var cls_btn = document.getElementsByClassName('cls_btn')[0]; //초기화 버튼을 지정. 배열로 참조됨
-    var result_btn = document.getElementsByClassName('result_btn')[0]; //결과 버튼을 지정. 배열로 참조됨
-
-    function clr() {
-        inp['result'].value = 0;
-    }
+    var frm = document.forms['cal']; //name이 'frm'인 form객체를 선택
+    var inputs = frm.getElementsByTagName('input'); //태그명이 input인 문서 객체를 지정. 배열로 참조됨
 
     //계산기 입력 처리 함수
-    function calc(value) {
-        //입력이 들어가면 0을 지움
-        if(inp['result'].value == 0) {
-            inp['result'].value = '';
+    function inp(value) {
+        if(frm['result'].value == 0) { //입력이 들어가면 0을 지움
+            frm['result'].value = '';
         }
     
-        //입력 값을 결과 창에 출력
-        inp['result'].value += value;
+        frm['result'].value += value; //입력 값을 결과 창에 추가하여 출력
     }
-    
-    //계산 결과 처리 함수
-    function my_result() {
-        var result = document.forms['cal']['result'];
-        var cal = eval(result.value);
-
-        //결과창에 표시
-        inp['result'].value = cal;
-    }
-
 
     //숫자 및 사칙 연산 버튼
     for(var i=0; i<inputs.length; i++) {
-        //숫자와 사칙 연산자만 입력 처리
         if(inputs[i].value != '=' && inputs[i].value != 'clear') {
-            inputs[i].onclick = function() {
-                calc(this.value); //헌재 클릭한 버튼 객체의 value 값을 calc 함수에 전달함
-            }
+            inputs[i].addEventListener('click', function() {
+                inp(this.value); //헌재 클릭한 버튼 객체의 value 값을 inp 함수에 전달함
+            });
         }
     }
 
-    //초기화 버튼
-    cls_btn.onclick = function() {
-        clr();
-    }
+    //계산기 초기화
+    frm['cls_btn'].addEventListener('click', function() {
+        frm['result'].value = 0;
+    });
 
     //결과 버튼
-    result_btn.onclick = function() {
+    frm['result_btn'].addEventListener('click', function() {
         try {
-            my_result();
+            frm['result'].value = eval(frm['result'].value); //사칙연산을 수행하고 그 값을 결과창에 표시
         }
         catch(err) {
-            var result = inp['result'];
-            result.value = '입력 오류';
+            frm['result'].value = '입력 오류';
         }
-    }
-    
-   
+    })
 
 }
