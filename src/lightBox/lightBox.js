f614f543bb057609fdace352fad11d6bbbe3ea7b
jessie <je_ss2@naver.com>
2018-04-25 01:35:54 +0900

    update

--- src/lightBox/lightBox.js
+++ src/lightBox/lightBox.js
@@ -1,31 +1,30 @@
 window.onload = function() {
-    //인디케이터 초기화
-    var indicator = document.querySelectorAll('.indicator button');
-    var lightbox = document.querySelector('#lightbox');
-    var block = document.querySelector('#block');
+    var lightbox = document.getElementById('#lightbox');
+    var block = document.getElementById('#block');
 }
 
-    //라이트 박스 표시
-    function lightbox_open(num) {
-        lightbox.setAttribute('class', 'active');
-        block.setAttribute('class', 'active');
-        change_img(num);
-        indicator[num-1].focus();
+//라이트 박스 표시
+function lightbox_open(num) {
+    var indicators = document.querySelectorAll('.indicator button');
+    lightbox.setAttribute('class', 'active'); //라이트박스 창 active
+    block.setAttribute('class', 'active'); //라이트박스 배경 active
+    change_img(num); //해당 이미지에만 active 클래스를 주기 위해 함수 호출
+    indicators[num-1].focus(); //해당 버튼에 css 주기 위해 focus
+}
+
+//라이트 박스 닫기
+function lightbox_close() {
+    lightbox.removeAttribute('class');
+    block.removeAttribute('class');
+}
+
+//이미지 전환 표시 함수
+function change_img(val) {
+    var imgs = document.querySelectorAll('figure > img');
+
+    for(var i=0; i<imgs.length; i++) {
+        imgs[i].removeAttribute('class');
     }
 
-    //라이트 박스 닫기
-    function lightbox_close() {
-        lightbox.removeAttribute('class');
-        block.removeAttribute('class');
-    }
-
-    //이미지 전환 표시 함수
-    function change_img(val) {
-        var imgs = document.querySelectorAll('figure > img');
-
-        for(var i=0; i<imgs.length; i++) {
-            imgs[i].removeAttribute('class');
-        }
-
-        imgs[val-1].setAttribute('class', 'active');
-    }
+    imgs[val-1].setAttribute('class', 'active');
+}
